8d6009753d336495a52dc93adf0686e3e22e71bc
최준영 <chad1994@skuniv.ac.kr>
2019-10-08 16:14:15 +0900

    프로그래머스 타겟 넘버

--- src/Programmers_BfsDfs1.java
+++ src/Programmers_BfsDfs1.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Programmers_BfsDfs1 {
+
+    public static void main(String args[]){
+        int[] numbers = {};
+        int target = 0;
+
+        solution(numbers,target);
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        answer = dfs(numbers,target,0,0);
+
+        return answer;
+    }
+
+    public static int dfs(int[] numbers,int target,int index,int num){
+        if(index == numbers.length) {
+            return num == target ? 1 : 0;
+        } else {
+            return dfs(numbers, target, index + 1, num + numbers[index])
+                    + dfs(numbers, target, index + 1, num - numbers[index]);
+        }
+    }
 }
