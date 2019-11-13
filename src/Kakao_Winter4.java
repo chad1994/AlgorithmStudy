7dd945a8f70b7bb987e6b567bb6fdc493903b865
chad1994 <chad1994@skuniv.ac.kr>
2019-11-13 13:24:03 +0900

    카카오 윈터인턴 코딩테스트

--- src/Kakao_Winter4.java
+++ src/Kakao_Winter4.java
@@ -1,4 +1,75 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class Kakao_Winter4 {
+
+    public static void main(String args[]){
+
+        long[] room_number = {30,40,30,41,100,1};
+        long k = 100;
+        long[] answer = solution2(k,room_number);
+
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
+
+    }
+
+    public static long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+
+        Set<Long> set = new HashSet<>();
+
+
+        for(int i=0;i<room_number.length;i++){
+            if (!set.contains(room_number[i])) {
+                answer[i] = room_number[i];
+                set.add(room_number[i]);
+            }else{
+                int count=1;
+                while(true){
+                    if(!set.contains(room_number[i]+count)){
+                        answer[i] = room_number[i]+count;
+                        set.add(room_number[i]+count);
+                        break;
+                    }else{
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static long[] solution2(long k, long[] room_number) {
+        Set<Long> set = new HashSet<>();
+
+        for(int i=0;i<room_number.length;i++)
+            room_number[i] = recursive(set,room_number[i]);
+
+
+        return room_number;
+    }
+
+
+
+    public static long recursive(Set<Long> set,long room_number){
+        long answer;
+        if(!set.contains(room_number)){
+            answer = room_number;
+            set.add(room_number);
+            return answer;
+        }else{
+            return recursive(set,room_number+1);
+        }
+    }
+
+
+
+
+
 }
--- src/Kakao_Winter5.java
+++ src/Kakao_Winter5.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Kakao_Winter5 {
+
+    public static void main(String args[]){
+        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+        int k = 3;
+
+        System.out.println(solution(stones,k));
+    }
+
+    public static int solution(int[] stones, int k) {
+        int answer = 0;
+
+
+        boolean isFinish = false;
+        while(!isFinish){
+            int count_k = 1;
+            for(int i=0;i<stones.length;i++){
+                if(stones[i]!=0){
+                    stones[i]--;
+                    count_k=1;
+                }else{
+                    count_k++;
+                    if(count_k>k){
+                        isFinish = true;
+                        break;
+                    }else{
+                        continue;
+                    }
+                }
+            }
+            if(!isFinish){
+                answer++;
+            }
+
+        }
+
+        return answer;
+    }
+
+
+
 }
+
