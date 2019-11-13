0ca3d7aa73d3e8b460f266dda96163e362641b24
chad1994 <chad1994@skuniv.ac.kr>
2019-11-13 13:25:13 +0900

    프로그래머스 - 124나라의 숫자

--- src/Programmers_124country.java
+++ src/Programmers_124country.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Programmers_124country {
+
+    public static void main(String args[]){
+
+        System.out.println(solution(10));
+    }
+
+    public static String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        int restNum=0;
+
+        for(int i=n;i>0;){
+            restNum = i%3;
+            i = i/3;
+            if (restNum == 0) {
+                restNum = 4;
+                i-=1;
+            }
+            sb.insert(0,restNum);
+        }
+
+        return sb.toString();
+    }
+
+
 }
