5a4a3309720d2cefde874d5fa63d12ad771f2b76
chad1994 <chad1994@skuniv.ac.kr>
2019-11-16 17:02:27 +0900

    프로그래머스 - 카펫

--- src/Programmers_BruteForce4.java
+++ src/Programmers_BruteForce4.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
 
+
+//카펫
 public class Programmers_BruteForce4 {
+
+    public static void main(String args[]){
+
+        int brown = 10;
+        int red = 2;
+
+        int[] answer = solution(brown,red);
+
+
+    }
+
+    public static int[] solution(int brown, int red) { //
+        int[] answer = new int[2];
+
+
+        int total = brown + red;
+        for(int x=3;;x++){
+            int y= total/x;
+            if(x*y==total && (x*2+y*2)==brown+4){
+                System.out.println(y+"/"+x);
+                answer[0]=y;
+                answer[1]=x;
+                break;
+            }
+        }
+
+        return answer;
+    }
 }
