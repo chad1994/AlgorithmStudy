6b6773a13d41a47e8806d6b27b533c5e49063e42
chad1994 <chad1994@skuniv.ac.kr>
2019-11-26 10:37:26 +0900

    프로그래머스 - 위장

--- src/Programmers_Hash3.java
+++ src/Programmers_Hash3.java
@@ -1,46 +1,34 @@
-
-
-// 베스트 앨범
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Programmers_Hash3 {
 
     public static void main(String args[]){
 
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500,600,150,800,2500};
-
-
-        int a[] = solution(genres,plays);
-
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(solution(clothes));
     }
 
-    public static int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+    public static int solution(String[][] clothes) {
+        int answer = 1;
 
-        Map<Integer,String> genresMap = new HashMap<>();
-        Map<Integer,Integer> playsMap = new HashMap<>();
-        Set set = new HashSet();
-        Map<Integer, String> genresRanking = new HashMap<>();
+        Map<String,Integer> map = new HashMap<>();
 
-        for(int i=0;i<genres.length;i++){
-            genresMap.put(i,genres[i]);
-            playsMap.put(i,plays[i]);
-            set.add(genres[i]);
+        for(int i=0;i<clothes.length;i++){
+            if(map.get(clothes[i][1])==null){
+                map.put(clothes[i][1],1);
+            }
+            else{
+                map.put(clothes[i][1],map.get(clothes[i][1])+1);
+            }
         }
 
-        for(int i=0;i<genres.length;i++){
-
+        for(int i : map.values()){
+            answer *= (i+1);
         }
 
-
-
-
+        answer-=1;
 
         return answer;
-
     }
-
 }
-
---
+++ src/Programmers_Hash4.java
@@ -0,0 +1,46 @@
+
+
+// 베스트 앨범
+
+import java.util.*;
+
+public class Programmers_Hash4 {
+
+    public static void main(String args[]){
+
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500,600,150,800,2500};
+
+
+        int a[] = solution(genres,plays);
+
+    }
+
+    public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+
+        Map<Integer,String> genresMap = new HashMap<>();
+        Map<Integer,Integer> playsMap = new HashMap<>();
+        Set set = new HashSet();
+        Map<Integer, String> genresRanking = new HashMap<>();
+
+        for(int i=0;i<genres.length;i++){
+            genresMap.put(i,genres[i]);
+            playsMap.put(i,plays[i]);
+            set.add(genres[i]);
+        }
+
+        for(int i=0;i<genres.length;i++){
+
+        }
+
+
+
+
+
+        return answer;
+
+    }
+
+}
+
