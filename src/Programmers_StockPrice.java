31e468c465e49e009d741c1a4ea98f2f7838c64e
chad1994 <chad1994@skuniv.ac.kr>
2020-04-28 20:45:47 +0900

    프로그래머스 - 주식 가격

---
+++ src/Programmers_StockPrice.java
@@ -0,0 +1,24 @@
+
+public class Programmers_StockPrice {
+
+    public static void main(String[] args) {
+        int[] prices = {1,2,3,2,3};
+        solution(prices);
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                answer[i]++;
+                if(prices[i]>prices[j]){
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
